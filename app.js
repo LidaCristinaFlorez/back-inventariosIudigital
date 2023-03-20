754d1833a1ac5dad43dde683759d23e60dbe6de4
Lida.Florez <lida.florez@convertic.com.co>
2023-03-20 17:26:13 -0500

    primer modulos

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+const tipoEquipo = require('./routes/tipoEquipo')
+const estado = require('./routes/estado')
+const usuario = require('./routes/usuario')
+const marca = require('./routes/marca')
+const inventario = require('./routes/inventario')
+
+
+app.use('/api/tiposequipos', tipoEquipo)
+app.use('/api/estados', estado)
+app.use('/api/usuarios', usuario)
+app.use('/api/marcas', marca)
+app.use('/api/inventarios', inventario)
+
+
+
+module.exports = app
